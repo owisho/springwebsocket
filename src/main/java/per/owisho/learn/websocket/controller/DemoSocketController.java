5905c723c8f6de3e138387485c374653ab3cd5dc
owisho <owisho2012@126.com>
2019-11-05 09:38:24 +0800

    自定义路由发送消息

--- src/main/java/per/owisho/learn/websocket/controller/DemoSocketController.java
+++ src/main/java/per/owisho/learn/websocket/controller/DemoSocketController.java
@@ -1,18 +1,49 @@
 package per.owisho.learn.websocket.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.server.Session;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.core.MessageSendingOperations;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class DemoSocketController {
 
+    @Autowired
+    MessageSendingOperations<String> messageSendingOperations;
+
     @MessageMapping("/test")
-    public void message(JSONObject json, Session session){
+    @SendTo("/topic/callback")
+    public String message(JSONObject json) {
         System.out.println(json);
         System.out.println("接收到socket消息");
+        return json.getString("message");
+}
+
+    @GetMapping("sendMsg")
+    @ResponseBody
+    public String sendMsg() {
+        Message<String> message = new Message<String>() {
+            @Override
+            public String getPayload() {
+                return "ssssssssssssssss";
+            }
+
+            @Override
+            public MessageHeaders getHeaders() {
+                return null;
+            }
+        };
+//        messageSendingOperations.send("", message);
+
+        this.messageSendingOperations.convertAndSend("/topic/callback", "sjon", message.getHeaders());
+        return "1";
     }
 }
